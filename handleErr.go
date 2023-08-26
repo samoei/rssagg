1a01814952551943619797e484321d32251acffa
Phil Samoei <phil@samphiltech.com>
2023-08-26 13:24:43 +0200

    Basic responses

--- handleErr.go
+++ handleErr.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func handleErr(w http.ResponseWriter, r *http.Request) {
-	repondWithJSON(w, 400, "Something went wrong")
+	respondWithError(w, 400, "Something went wrong")
 }
